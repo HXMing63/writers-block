aff8f70f9cca48787a746f560f2265da75057d92
Mervin <mervinsc.uy@gmail.com>
2023-11-20 21:13:09 +0800

    Updated BoxThreeDots name to ThreeDots. Fixed the page size issue of landing page.

--- writersblock-front/src/components/home/ViewHome.js
+++ writersblock-front/src/components/home/ViewHome.js
@@ -2,51 +2,29 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const ViewHome = () => {
-  const navigate = useNavigate();
-  return (
-    <div className="flex flex-col items-center h-screen bg-gray-100">
-      <h1 className="text-6xl font-bold text-gray-900 mb-8 hidden sm:block">
-        Writer's Block
-      </h1>
-      <h2 className="text-2xl font-semibold text-gray-700 mb-8 hidden sm:block">
-        Overcoming writer's block, one word at a time
-      </h2>
-      <div className="flex flex-row w-full max-w-md space-x-4 mt-8">
-        <div
-          className="flex-1 bg-white rounded-lg shadow-md p-8 flex flex-col items-center justify-center hover:shadow-2xl hover:cursor-pointer hover:bg-gray-100"
-          onClick={() => navigate("/viewBook")}
-        >
-          <h2 className="text-lg font-bold text-gray-900 mb-4 text-center">
-            Book
-          </h2>
-        </div>
-        <div
-          className="flex-1 bg-white rounded-lg shadow-md p-8 flex flex-col items-center justify-center hover:shadow-2xl hover:cursor-pointer hover:bg-gray-100"
-          onClick={() => {navigate("/viewStoryChar")}}
-        >
-          <h2 className="text-lg font-bold text-gray-900 mb-4 text-center">
-            Character
-          </h2>
-        </div>
-        <div
-          className="flex-1 bg-white rounded-lg shadow-md p-8 flex flex-col items-center justify-center hover:shadow-2xl hover:cursor-pointer hover:bg-gray-100"
-          onClick={() => {navigate("/viewPlace")}}
-        >
-          <h2 className="text-lg font-bold text-gray-900 mb-4 text-center">
-            Place
-          </h2>
-        </div>
-        <div
-          className="flex-1 bg-white rounded-lg shadow-md p-8 flex flex-col items-center justify-center hover:shadow-2xl hover:cursor-pointer hover:bg-gray-100"
-          onClick={() => {navigate("/viewStorySetting")}}
-        >
-          <h2 className="text-lg font-bold text-gray-900 mb-4 text-center">
-            Setting
-          </h2>
-        </div>
-      </div>
-    </div>
-  );
+	const navigate = useNavigate();
+	return (
+		<div className="bg-gray-100 h-full">
+			<div className="flex flex-col justify-center items-center space-y-4">
+				<h1 className="text-2xl md:text-6xl font-bold text-gray-900">
+					Writer's Block
+				</h1>
+				<h2 className="text-lg md:text-2xl font-semibold text-gray-700">
+					Overcoming writer's block, one word at a time
+				</h2>
+			</div>
+			<div className="flex justify-center items-center space-x-2 py-8">
+				<div
+					className="bg-white rounded-lg shadow-md p-8 flex justify-center items-center hover:shadow-2xl hover:cursor-pointer hover:bg-gray-100"
+					onClick={() => navigate("/viewBook")}
+				>
+					<h2 className="text-lg font-bold text-gray-900 mb-4 text-center">
+						Book
+					</h2>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ViewHome;
