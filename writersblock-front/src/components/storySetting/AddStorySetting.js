63a3f6aa8ffe88729342b3028a15b089e5b397bd
Mervin <mervinsc.uy@gmail.com>
2023-12-28 15:45:28 +0800

    Renamed ViewAddPage to ViewAddPageTemplate. Added AddSideComponentTemplate. Added a title to the view section of ViewPlace, ViewStoryChar, ViewStorySetting. Modified AddPlace, AddStoryChar, AddStorySetting to use AddSideComponentTemplate.

--- writersblock-front/src/components/storySetting/AddStorySetting.js
+++ writersblock-front/src/components/storySetting/AddStorySetting.js
@@ -1,72 +1,58 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import StorySettingService from "../../services/StorySettingService";
 import StorySettingForm from "./StorySettingForm";
+import AddSideComponent from "../assets/template/AddSideComponentTemplate";
 
 const AddStorySetting = ({ fetchData }) => {
-  const [query, setQuery] = useState("");
-  const [places, setPlaces] = useState([]);
-  const [storySetting, setStorySetting] = useState({
-    id: "",
-    place: null,
-    time: "",
-  });
+	const [query, setQuery] = useState("");
+	const [places, setPlaces] = useState([]);
+	const [storySetting, setStorySetting] = useState({
+		id: "",
+		place: null,
+		time: "",
+	});
 
-  const saveStorySetting = (e) => {
-    e.preventDefault();
+	const saveStorySetting = (e) => {
+		e.preventDefault();
 
-    StorySettingService.saveStorySetting(storySetting)
-      .then((response) => {
-        console.log(response);
-        fetchData();
-        resetForm();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+		StorySettingService.saveStorySetting(storySetting)
+			.then((response) => {
+				console.log(response);
+				fetchData();
+				resetForm();
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
-  const resetForm = (e) => {
-    e.preventDefault();
-    setStorySetting({
-      id: "",
-      place: null,
-      time: "",
-    });
-    setQuery("");
-  };
+	const resetForm = (e) => {
+		e.preventDefault();
+		setStorySetting({
+			id: "",
+			place: null,
+			time: "",
+		});
+		setQuery("");
+	};
 
-  return (
-    <div className="flex flex-shrink max-w-md shadow border-b mx-auto">
-      <div className="px-4 py-4">
-        <div className="font-thin text-2xl tracking-wider">
-          <h1>Add New Setting</h1>
-        </div>
-        <StorySettingForm
-          storySetting={storySetting}
-          setStorySetting={setStorySetting}
-          query={query}
-          setQuery={setQuery}
-          places={places}
-          setPlaces={setPlaces}
-          prev={""}
-        ></StorySettingForm>
-        <div className="flex items-center justify-end h-14 w-full my-8 space-x-2 pt-4">
-          <button
-            className="rounded-md text-white font-semibold bg-red-400 px-2 py-2 hover:bg-red-700"
-            onClick={resetForm}
-          >
-            Clear
-          </button>
-          <button
-            className="rounded-md text-white font-semibold bg-green-400 px-2 py-2 hover:bg-green-700"
-            onClick={saveStorySetting}
-          >
-            Save
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<AddSideComponent
+			title={"Add New Story Setting"}
+			saveFunc={saveStorySetting}
+			resetForm={resetForm}
+		>
+			<StorySettingForm
+				storySetting={storySetting}
+				setStorySetting={setStorySetting}
+				query={query}
+				setQuery={setQuery}
+				places={places}
+				setPlaces={setPlaces}
+				prev={""}
+			></StorySettingForm>
+		</AddSideComponent>
+	);
 };
 
 export default AddStorySetting;
--- writersblock-front/src/components/storySetting/ViewStorySetting.js
+++ writersblock-front/src/components/storySetting/ViewStorySetting.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
 import AddStorySetting from "./AddStorySetting";
-import StorySetting from "./StorySetting";
 import StorySettingService from "../../services/StorySettingService";
 import EditStorySetting from "./EditStorySetting";
-import ViewAddPage from "../assets/template/ViewAddPage";
+import ViewAddPage from "../assets/template/ViewAddPageTemplate";
 import ImageCard from "../assets/components/ImageCard";
 import ImageService from "../../services/ImageService";
 import ModalTemplate from "../assets/components/ModalTemplate";
@@ -11,16 +10,10 @@ import ModalTemplate from "../assets/components/ModalTemplate";
 const ViewStorySetting = () => {
 	const [isLoading, setIsLoading] = useState(true);
 	const [storySettings, setStorySettings] = useState(null);
-	const [sortOrder, setSortOrder] = useState({
-		column: "",
-		direction: "asc",
-	});
 	const [modal, setModal] = useState({
 		isVisible: false,
 		storySetting: null,
 	});
-
-	const handleSort = (column) => {};
 
 	const fetchData = async () => {
 		setIsLoading(true);
@@ -51,32 +44,42 @@ const ViewStorySetting = () => {
 		setModal({ isVisible: true, storySetting: storySetting });
 	};
 
-  const formatToCardTitle = (storySetting) => {
-    return (
-      <>
-        <div className="w-28 truncate text-lg">{storySetting.place.name}</div>
-        <div className="w-28 truncate text-xs text-gray-200 tracking-wider">{storySetting.time}</div>
-      </>
-    );
-  }
+	const formatToCardTitle = (storySetting) => {
+		return (
+			<>
+				<div className="w-28 truncate text-lg">{storySetting.place.name}</div>
+				<div className="w-28 truncate text-xs text-gray-200 tracking-wider">
+					{storySetting.time}
+				</div>
+			</>
+		);
+	};
 
 	return (
 		<ViewAddPage>
-			<div className="flex flex-wrap justify-center">
-				{isLoading ? (
-					<div className="p-4 text-thin-wider-2xl text-gray-200">
-						Loading...
-					</div>
-				) : (
-					storySettings.map((storySetting) => (
-						<div onClick={() => editStorySetting(storySetting)} key={storySetting.id}>
-							<ImageCard
-								imgSrc={ImageService.createImgURL(storySetting.place.img)}
-								cardName={formatToCardTitle(storySetting)}
-							/>
+			<div className="flex flex-col">
+				<div className="p-4 justify-start">
+					<span className="text-thin-wider-2xl text-gray-200">View Story Settings</span>
+				</div>
+				<div className="flex flex-wrap justify-center">
+					{isLoading ? (
+						<div className="p-4 text-thin-wider-2xl text-gray-200">
+							Loading...
 						</div>
-					))
-				)}
+					) : (
+						storySettings.map((storySetting) => (
+							<div
+								onClick={() => editStorySetting(storySetting)}
+								key={storySetting.id}
+							>
+								<ImageCard
+									imgSrc={ImageService.createImgURL(storySetting.place.img)}
+									cardName={formatToCardTitle(storySetting)}
+								/>
+							</div>
+						))
+					)}
+				</div>
 			</div>
 			<div className="w-fit mx-auto flex">
 				<AddStorySetting fetchData={fetchData} />
@@ -93,73 +96,6 @@ const ViewStorySetting = () => {
 				/>
 			</ModalTemplate>
 		</ViewAddPage>
-		// <div className="container mx-auto flex flex-wrap max-w-fit overflow-x-hidden">
-		//   <div className="grid grid-cols-1 2xl:grid-cols-3 gap-20 my-6">
-		//     <div className="2xl:col-span-2 overflow-x-auto">
-		//       <div className="px-4 font-thin text-2xl tracking-wider">
-		//         <h1>Story Setting View</h1>
-		//       </div>
-		//       <div className="flex shadow border-b my-4">
-		//         <table className="min-w-full ">
-		//           <thead className="bg-gray-200">
-		//             <tr className="text-left font-medium text-gray-500 uppercase tracking-wider">
-		//               <th
-		//                 className="py-3 px-6 cursor-pointer"
-		//                 onClick={() => handleSort("place")}
-		//               >
-		//                 Place
-		//                 {sortOrder.column === "place" && (
-		//                   <span>{sortOrder.direction === "asc" ? " ▲" : " ▼"}</span>
-		//                 )}
-		//               </th>
-		//               <th
-		//                 className="py-3 px-6 w-1/4 cursor-pointer"
-		//                 onClick={() => handleSort("time")}
-		//               >
-		//                 Time
-		//                 {sortOrder.column === "time" && (
-		//                   <span>{sortOrder.direction === "asc" ? " ▲" : " ▼"}</span>
-		//                 )}
-		//               </th>
-		//               <th className="text-right py-3 px-6">Action</th>
-		//             </tr>
-		//           </thead>
-
-		//           {!isLoading && (
-		//             <tbody className="bg-white">
-		//               {storySettings &&
-		//                 storySettings.map((storySetting) => (
-		//                   <StorySetting
-		//                     storySetting={storySetting}
-		//                     deleteStorySetting={deleteStorySetting}
-		//                     editStorySetting={editStorySetting}
-		//                     key={storySetting.id}
-		//                   />
-		//                 ))}
-		//             </tbody>
-		//           )}
-		//         </table>
-		//       </div>
-		//     </div>
-		//     <div className="2xl:col-span-1">
-		//       <AddStorySetting fetchData={fetchData} />
-		//     </div>
-		//   </div>
-		//   {modal.isVisible && (
-		//     <div className="flex flex-shrink fixed inset-0 z-10 overflow-y-hidden overflow-x-hidden justify-center">
-		//       <div className="fixed inset-0 w-full h-full bg-black opacity-40"></div>
-		//       <div className="flex items-center min-h-screen px-4 py-8">
-		//         <div className="relative max-w-fit mx-auto bg-white rounded-md shadow-lg">
-		//           <EditStorySetting
-		//             toEdit={modal.storySetting}
-		//             setModal={setModal}
-		//             fetchData={fetchData}
-		//           />
-		//         </div>
-		//       </div>
-		//     </div>
-		//   )}
-		// </div>
 	);
 };
 
